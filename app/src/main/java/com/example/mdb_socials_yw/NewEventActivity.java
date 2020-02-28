f8188a46323f91cd75c9b53dc9cfcf20a0dce2c6
William Vavrik <whvb1@berkeley.edu>
2020-02-28 15:50:58 -0800

    created a new activity, layout file, added in Android Manifest

---
+++ app/src/main/java/com/example/mdb_socials_yw/NewEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.mdb_socials_yw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+}
